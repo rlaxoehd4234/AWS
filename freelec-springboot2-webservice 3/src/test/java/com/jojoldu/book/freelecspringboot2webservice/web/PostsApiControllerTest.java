a5a034e0443877eabcea8f1547c8d7531801adad
kimtaedong <rlaxoehd4234@naver.com>
2022-12-15 19:53:20 +0900

    2022/12/15

--- freelec-springboot2-webservice 3/src/test/java/com/jojoldu/book/freelecspringboot2webservice/web/PostsApiControllerTest.java
+++ freelec-springboot2-webservice 3/src/test/java/com/jojoldu/book/freelecspringboot2webservice/web/PostsApiControllerTest.java
@@ -1,13 +1,20 @@
 package com.jojoldu.book.freelecspringboot2webservice.web;
 
+import com.jojoldu.book.freelecspringboot2webservice.domain.posts.Posts;
 import com.jojoldu.book.freelecspringboot2webservice.domain.posts.PostsRepository;
+import com.jojoldu.book.freelecspringboot2webservice.web.dto.PostsSaveRequestDto;
 import org.junit.After;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -27,5 +34,34 @@ public class PostsApiControllerTest {
     @After
     public void tearDown() throws Exception {
         postsRepository.deleteAll();
+    }
+
+    @Test
+    public void Ports_등록된다() throws Exception {
+        //given
+
+        String title = "title";
+        String content = "content";
+        PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
+                .title(title)
+                .content(content)
+                .author("author")
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts";
+
+        //when
+
+
+        ResponseEntity<Long> reponseEnitiy = restTemplate.postForEntity(url, requestDto, Long.class);
+
+
+        //then
+        assertEquals(reponseEnitiy.getStatusCode() , HttpStatus.OK);
+
+        List<Posts> all = postsRepository.findAll();
+        assertEquals(all.get(0).getTitle(),title);
+        assertEquals(all.get(0).getContent() , content);
+
     }
 }
