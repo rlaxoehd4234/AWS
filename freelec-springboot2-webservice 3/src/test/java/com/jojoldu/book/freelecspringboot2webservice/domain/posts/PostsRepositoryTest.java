7355f68ec203af0e44900ce245a63e6cd16cb035
kimtaedong <rlaxoehd4234@naver.com>
2022-12-15 19:33:54 +0900

    2022/12/15

--- freelec-springboot2-webservice 3/src/test/java/com/jojoldu/book/freelecspringboot2webservice/domain/posts/PostsRepositoryTest.java
+++ freelec-springboot2-webservice 3/src/test/java/com/jojoldu/book/freelecspringboot2webservice/domain/posts/PostsRepositoryTest.java
@@ -2,12 +2,17 @@ package com.jojoldu.book.freelecspringboot2webservice.domain.posts;
 
 import org.junit.After;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class PostsRepositoryTest {
 
     @Autowired
---
+++ freelec-springboot2-webservice 3/src/test/java/com/jojoldu/book/freelecspringboot2webservice/web/PostsApiControllerTest.java
@@ -0,0 +1,31 @@
+package com.jojoldu.book.freelecspringboot2webservice.web;
+
+import com.jojoldu.book.freelecspringboot2webservice.domain.posts.PostsRepository;
+import org.junit.After;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PostsApiControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @After
+    public void tearDown() throws Exception {
+        postsRepository.deleteAll();
+    }
+}
