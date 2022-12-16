d220a675d3c4188bf9634663a33507ef94f0a4af
kimtaedong <rlaxoehd4234@naver.com>
2022-12-16 16:39:53 +0900

    PostsRepository 테스트 완료

--- freelec-springboot2-webservice 3/src/main/java/com/jojoldu/book/freelecspringboot2webservice/FreelecSpringboot2WebserviceApplication.java
+++ freelec-springboot2-webservice 3/src/main/java/com/jojoldu/book/freelecspringboot2webservice/FreelecSpringboot2WebserviceApplication.java
@@ -3,9 +3,9 @@ package com.jojoldu.book.freelecspringboot2webservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@SpringBootConfiguration
 public class FreelecSpringboot2WebserviceApplication {
 
 	public static void main(String[] args) {
