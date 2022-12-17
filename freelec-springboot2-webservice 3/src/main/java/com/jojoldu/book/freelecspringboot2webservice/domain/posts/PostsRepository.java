597969d26b9853278adecb7631b94ec0c511005c
kimtaedong <rlaxoehd4234@naver.com>
2022-12-17 22:22:06 +0900

    repo 정리

--- freelec-springboot2-webservice 3/src/main/java/com/jojoldu/book/freelecspringboot2webservice/domain/posts/PostsRepository.java
+++ freelec-springboot2-webservice 3/src/main/java/com/jojoldu/book/freelecspringboot2webservice/domain/posts/PostsRepository.java
@@ -2,7 +2,7 @@ package com.jojoldu.book.freelecspringboot2webservice.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostsRepository extends JpaRepository<Posts,Long> {
+public interface PostsRepository extends JpaRepository<Posts,Long> { // Repository 는 dao 이다 스프링에서는 DAO 대신에 repo 가 쓰인다.
 
 
 }
